7a3f79cde1d4c912ab0e883f3f2c7136c3783f6c
nata20019 <nbernikova68@gmail.com>
2025-06-11 23:02:48 +0200

    mod42

--- src/components/ContactsSearch.jsx
+++ src/components/ContactsSearch.jsx
@@ -1,32 +1,28 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 
-export class ContactsSearch extends Component {
-  static propTypes = {
-    filter: PropTypes.string.isRequired,
-    onFilterChange: PropTypes.func.isRequired,
-  };
+const ContactsSearch = ({ filter, onFilterChange }) => {
+  const searchInputId = nanoid();
 
-  render() {
-    const { filter, onFilterChange } = this.props;
-    const searchInputId = nanoid();
+  return (
+    <div className="input-group mb-3">
+      <label htmlFor={searchInputId} className="name-to-finde">
+        Find contacts by name
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search contact"
+        id={searchInputId}
+        value={filter}
+        onChange={onFilterChange}
+      />
+    </div>
+  );
+};
+ContactsSearch.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onFilterChange: PropTypes.func.isRequired,
+};
 
-    return (
-      <div className="input-group mb-3">
-        <label htmlFor={searchInputId} className="name-to-finde">
-          Find contacts by name
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Search contact"
-          id={searchInputId}
-          value={filter}
-          onChange={onFilterChange}
-        />
-      </div>
-    );
-  }
-}
 export default ContactsSearch;
